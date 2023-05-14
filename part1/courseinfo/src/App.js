52bde777660e7512d7c1cd414f7da9df12620924
Gustavo Jorge Nau <gustavo.22jn@gmail.com>
2023-05-14 14:00:05 -0300

    add courseinfo exercise

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,57 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => <Part part={part.name} exercises={part.exercises} />)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.map(part => part.exercises).reduce((a, b) => a + b, 0);
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App;
